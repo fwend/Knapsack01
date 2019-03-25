6824eceff7d28a0b0c248d944f45d43e5fbd1cfb
Jos van Uden <josvanuden@gmail.com>
2019-03-25 18:54:00 +0100

    larger input

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
-const values = [60, 100, 120];
-const weights = [10, 20, 30];
-const max = 50;
+const values = [150, 35, 200, 160, 60, 45, 60, 40, 30, 10, 70, 30, 15, 10, 40, 70, 75, 80, 20, 12, 50, 10];
+const weights = [9, 13,153, 50, 15, 68, 27, 39, 23, 52, 11, 32, 24, 48, 73, 42, 43, 22,  7, 18, 4, 30];
+const max = 400;
+
+// Total weight and value: [396, 1030]
 
 const knapsack01 = (totalValue, totalWeight, index) => {
     if (totalWeight > max) {
